8be6251e445492ca44c54f2c8dc3b9c4551d5d7f
corinnekrych <corinnekrych@gmail.com>
2013-06-13 15:44:51 +0200

    commetn bootstrap

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,20 +1,20 @@
-import threecirclesweb5.Place
-import threecirclesweb5.Friend
+//import threecirclesweb5.Place
+//import threecirclesweb5.Friend
 
 class BootStrap {
 
     def init = { servletContext ->
-        Friend fabrice = new Friend(firstname: "Fabrice", lastname: "Matrat", registrationDate: new Date())
-        fabrice.save()
-
-        Friend sebastien = new Friend(firstname: "Sebastien", lastname: "Blanc", registrationDate:  new Date())
-        sebastien.save()
-
-        Place nice = new Place(description: "Nice", latitude:43.7, longitude: 7.2 )
-        nice.save()
-
-        Place beziers = new Place(description: "Béziers", latitude:43.34, longitude: 3.24 )
-        beziers.save()
+//        Friend fabrice = new Friend(firstname: "Fabrice", lastname: "Matrat", registrationDate: new Date())
+//        fabrice.save()
+//
+//        Friend sebastien = new Friend(firstname: "Sebastien", lastname: "Blanc", registrationDate:  new Date())
+//        sebastien.save()
+//
+//        Place nice = new Place(description: "Nice", latitude:43.7, longitude: 7.2 )
+//        nice.save()
+//
+//        Place beziers = new Place(description: "Béziers", latitude:43.34, longitude: 3.24 )
+//        beziers.save()
 
     }
     def destroy = {
